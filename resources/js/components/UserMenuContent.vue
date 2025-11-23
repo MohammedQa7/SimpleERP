0c0cba4d959d8ace89f65e47a14d03066c3817b9
MohammedQa7 <slizzertheflizzer677@gmail.com>
2025-11-23 04:25:00 +0200

    adding the ui component for switching accounts

--- resources/js/components/UserMenuContent.vue
+++ resources/js/components/UserMenuContent.vue
@@ -3,8 +3,10 @@ import UserInfo from '@/components/UserInfo.vue';
 import { DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator } from '@/components/ui/dropdown-menu';
 import type { User } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { LogOut, Settings } from 'lucide-vue-next';
-
+import { CircleIcon, LogOut, Plus, Settings } from 'lucide-vue-next';
+import AccountSwticher from './AccountSwitcher.vue';
+import Separator from './ui/separator/Separator.vue';
+import Badge from './ui/badge/Badge.vue';
 interface Props {
     user: User;
 }
@@ -13,10 +15,31 @@ defineProps<Props>();
 </script>
 
 <template>
-    <DropdownMenuLabel class="p-0 font-normal">
-        <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-            <UserInfo :user="user" :show-email="true" />
+    <DropdownMenuLabel class="p-0 font-normal space-y-2">
+        <div>
+            <span class="flex  p-1 text-left text-xs font-bold">Accounts</span>
+            <div class="flex items-center gap-2  text-left text-sm mb-2 p-1">
+                <UserInfo :user="user" :show-email="true" />
+                <Badge class="bg-muted px-1 py-0.5 pointer-events-none">
+                    <CircleIcon :size="8" class="fill-green-700 " />
+                </Badge>
+            </div>
+
+            <!-- Other logged in accounts -->
+            <AccountSwticher />
         </div>
+
+        <Separator />
+
+        <!-- Sign in with new account section -->
+        <DropdownMenuItem :as-child="true">
+            <Link :href="route('accounts.switch.login')">
+                <Plus class="size-4" />
+                <div class="text-muted-foreground">
+                    Sign in with new account
+                </div>
+            </Link>
+        </DropdownMenuItem>
     </DropdownMenuLabel>
     <DropdownMenuSeparator />
     <DropdownMenuGroup>
